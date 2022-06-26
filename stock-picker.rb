2053dfe49a350cb3a8e8f49c3694e33472bb2c4f
Qwib-Qwib <jp93rom@orange.fr>
2022-06-26 16:17:49 +0200

    Code the first and second levels of the result hash. The current return is a hash of buying dates (first level), the value of each being a hash of their possible returns depending on selling date (second level).

---
+++ stock-picker.rb
@@ -0,0 +1,23 @@
+def stock_picker (prices_array) #This method is the intializer. It indicates which buying date we're currently considering. It calls another method to calculate the gains and losses with that date in mind.
+  i = 0
+  prices_array.reduce({}) do |initial_day, current_buy_price|
+    initial_day["Buy on day #{prices_array.index(current_buy_price) + 1}"] = sell_values_calculator(prices_array, buy_date_index = prices_array.index(current_buy_price))
+  i += 1
+  initial_day
+  end
+end
+
+def sell_values_calculator (prices_array, buy_date_index)
+  i = 0 #It's important to work with an iterator because by default reduce doesn't actually remember WHERE It iterates in the collection, which can lead to "funny" results when the same value appears several times.
+  prices_array.reduce({}) do |potential_gains_and_losses, current_sell_price|
+    if i <= buy_date_index  #This condition excludes all selling values for days before the current buying date.
+      i += 1
+    else
+      potential_gains_and_losses["Sell on day #{i + 1}"] = prices_array[buy_date_index] - current_sell_price
+      i += 1
+    end
+    potential_gains_and_losses
+  end
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
