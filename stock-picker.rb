9b4c4e5e8464027728fcc0918f568d7ea40232c0
Qwib-Qwib <jp93rom@orange.fr>
2022-06-26 16:23:14 +0200

    Improve the comments, fix the keys for both levels of the hash so that they correctly start counting from day 0, as requested in the README.

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,7 +1,7 @@
-def stock_picker (prices_array) #This method is the intializer. It indicates which buying date we're currently considering. It calls another method to calculate the gains and losses with that date in mind.
+def stock_picker (prices_array) #This method is the intializer. It indicates which buying date we're currently considering.
   i = 0
   prices_array.reduce({}) do |initial_day, current_buy_price|
-    initial_day["Buy on day #{prices_array.index(current_buy_price) + 1}"] = sell_values_calculator(prices_array, buy_date_index = prices_array.index(current_buy_price))
+    initial_day["Buy on day #{prices_array.index(current_buy_price)}"] = sell_values_calculator(prices_array, buy_date_index = prices_array.index(current_buy_price))  #A method is called to calculate the gains and losses with that specific buying date in mind.
   i += 1
   initial_day
   end
@@ -13,7 +13,7 @@ def sell_values_calculator (prices_array, buy_date_index)
     if i <= buy_date_index  #This condition excludes all selling values for days before the current buying date.
       i += 1
     else
-      potential_gains_and_losses["Sell on day #{i + 1}"] = prices_array[buy_date_index] - current_sell_price
+      potential_gains_and_losses["Sell on day #{i}"] = prices_array[buy_date_index] - current_sell_price
       i += 1
     end
     potential_gains_and_losses
